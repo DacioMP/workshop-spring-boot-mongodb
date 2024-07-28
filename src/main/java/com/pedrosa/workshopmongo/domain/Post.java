deec1a84308578d47f2c5807bf36a150bf063e97
Matheus Pedrosa <matheuspedrosa.contato@gmail.com>
2024-07-28 00:34:00 -0400

    Adding comments to posts

--- src/main/java/com/pedrosa/workshopmongo/domain/Post.java
+++ src/main/java/com/pedrosa/workshopmongo/domain/Post.java
@@ -2,12 +2,15 @@ package com.pedrosa.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.pedrosa.workshopmongo.dto.AuthorDTO;
+import com.pedrosa.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {}
 	
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -87,5 +100,5 @@ public class Post implements Serializable {
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}	
+	}
 }
